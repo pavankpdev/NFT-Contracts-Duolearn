dd74656bc42a7e9860ed9f4ed559b606aaffa233
pavanleton <pavankumarkpkp@gmail.com>
2022-09-13 10:06:52 +0530

    update: add Counters to track NFT totalsupply

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -1,10 +1,14 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract DuoNFT is ERC721 {
     constructor(string memory _name, string memory _symbol)
-        ERC721(_name, _symbol) {}
+    ERC721(_name, _symbol) {}
+
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
 
     mapping (uint => string) private _tokenUri;
 
@@ -20,9 +24,15 @@ contract DuoNFT is ERC721 {
     function mint (address recipient, uint tokenId, string memory tokenUri) public {
         _mint(recipient, tokenId);
         _setTokenUri(tokenUri, tokenId);
+        Counters.increment(_tokenIds);
     }
 
     function tokenURI (uint tokenId) public view virtual override isTokenExist(tokenId) returns(string memory) {
         return _tokenUri[tokenId];
     }
+
+    function totalSupply () public view returns(uint) {
+        return Counters.current(_tokenIds);
+    }
+
 }
